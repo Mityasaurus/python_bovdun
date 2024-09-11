ba411b27affce4e564bb8807e34c0a3bbf22078d
Mityasaurus <dmitriybovdun5@gmail.com>
2024-09-11 13:31:35 +0300

    add lab 2 code

---
+++ lab2/lab_2_Bovdun.py
@@ -0,0 +1,27 @@
+# Task 1
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+# Task 2
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#Task 3
+print("I'm student")
+
+#Task 4
+print('"I\'m"\n""learning""\n"""Python"""')
+
+#Task 5
+print(5 * 8**2 + 0 * 8**1 + 0 * 8**0)
+print(int("500", 8))
+
+#Task 6
+print(7 * 16**2 + 7 * 16**1 + 7 * 16**0)
+print(int("777", 16))
